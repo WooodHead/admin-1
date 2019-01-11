87f0d99920d5655e5aec7da600b7dce7f399959e
Adam Zygadlewicz <adam@zygadlewicz.com>
2019-01-11 15:04:16 +0100

    Updates

---
+++ precache-manifest.e9a033b93d696441efb362d1cccfc6be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "60d68cea2c660507df10",
+    "url": "/static/js/main.60d68cea.chunk.js"
+  },
+  {
+    "revision": "39ae406d2b108028ed97",
+    "url": "/static/js/1.39ae406d.chunk.js"
+  },
+  {
+    "revision": "60d68cea2c660507df10",
+    "url": "/static/css/main.abd9b707.chunk.css"
+  },
+  {
+    "revision": "a511b76980f43325a5c1717eddeead85",
+    "url": "/index.html"
+  }
+];
